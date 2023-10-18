a542dfbc419cb5b2e2a352e2530beda7ffaac7ac
Truong Nguyen <tnguyen10@my.hpu.edu>
2023-10-18 09:09:13 +0000

    fix form

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :due_date, presence: true, comparison: { greater_than: DateTime.current.to_date, message: "must be later than current date"}
 end
