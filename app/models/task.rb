567461aea6e2fdb5c3e9ed76263c40dcda438a6f
Truong Nguyen <tnguyen10@my.hpu.edu>
2023-10-24 01:29:41 +0000

    revise date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :due_date, presence: true, comparison: { greater_than: DateTime.current.to_date, message: "must be later than current date"}
+  validates :due_date, presence: true, comparison: { greater_than_or_equal_to: DateTime.current.to_date, message: "must be later than current date"}
 end
